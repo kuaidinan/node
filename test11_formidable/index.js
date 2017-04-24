6d603a6b731f55e2dde32c0ca6673abd885231f4
xuqiang <352789535@qq.com>
2017-04-24 17:32:48 +0800

    formidable 文件的上传

---
+++ test11_formidable/index.js
@@ -0,0 +1,51 @@
+var http = require('http');
+var formidable = require('formidable');
+var server = http.createServer(function(req,res){
+  switch(req.method){
+    case 'GET':
+      show(req,res);
+      break;
+    case "POST":
+      upload(req,res);
+      break;
+  }
+});
+
+function show(req,res){
+  var html = `<html>
+    <form action="/" method="post" enctype="multipart/form-data">
+      <p><input type="text" name="name"/></p>
+      <p><input type="file" name="file"/></p>
+      <p><input type="submit" value="Upload"/></p>
+    </form>
+  </html>`;
+  res.setHeader('Content-Type','text/html');
+  res.setHeader('Content-Length',Buffer.byteLength(html));
+  res.end(html);
+}
+
+function upload(req,res){
+  if(!isFormData(req)){
+    res.statusCode = 400;
+    res.end('Bad Request:expecting multipart/form-data');
+    return;
+  }
+  var form = new formidable.IncomingForm();
+  form.parse(req,function(err,fields,files){
+    console.log(fields);
+    console.log(files);
+    res.end('upload complete!');
+  })
+  form.on('progress',function(bytesReceived,bytesExpected){
+    var percent = Math.floor(bytesReceived / bytesExpected*100);
+    console.log(percent);
+  })
+}
+
+function isFormData(req){
+  var type = req.headers['content-type'] || '';
+  console.log(type);
+  return 0 == type.indexOf('multipart/form-data');
+}
+
+server.listen('3000');
