b6e592f409bf9235582a106b7b0ea29223ec0941
xuqiang <352789535@qq.com>
2017-05-11 18:29:17 +0800

    update ejs

--- test24_ejs/routes/list.js
+++ test24_ejs/routes/list.js
@@ -1,7 +1,17 @@
+let show = [];
+show.push({
+  url:'http://ocjvsx2zd.bkt.clouddn.com/o-b-6406.jpg',
+  name:'第八号当铺'
+});
+show.push({
+  url:'http://ocjvsx2zd.bkt.clouddn.com/o-b-6337.jpg',
+  name:'2017周杰伦地表最强演唱会'
+})
+
 var express = require('express');
 var router = express.Router();
 router.get('/', function (req, res, next) {
-  res.render('list', { test: '111' });
+  res.render('list', { test: '111' ,show:show});
 });
 
 module.exports = router;
