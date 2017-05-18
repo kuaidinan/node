faa9d357870624d0d9c349b6846fb45e06dce7c0
xuqiang <352789535@qq.com>
2017-05-18 15:08:50 +0800

    遍历对象的探索

--- test26_shoutbox/lib/user.js
+++ test26_shoutbox/lib/user.js
@@ -1,16 +1,17 @@
-var reids = require('redis');
+var redis = require('redis');
 var bcrypt = require('bcrypt');
 var db = redis.createClient();
+db.auth('hello');
 
-module.exports = User;
+module.exports = Users;
 
-function User(obj) {
+function Users(obj) {
   for (var key in obj) {
     this[key] = obj[key];
   }
 }
 
-User.prototype.save = function (fn) {
+Users.prototype.save = function (fn) {
   if (this.id){
     this.update(fn);
   } else {
@@ -26,18 +27,25 @@ User.prototype.save = function (fn) {
   }
 }
 
-User.prototype.update = function(fn){
+Users.prototype.update = function(fn){
   var user = this;
   var id = user.id;
   db.set('user:id:' + user.name,id,function(err){
     if (err) return fn(err);
-    db.hmset('user:' + id, user, function(err){
+    var userObj = {};
+    for(var key in user){
+      if(user.hasOwnProperty(key)){
+        userObj[key] = user[key];
+      }
+    }
+    console.log(userObj);
+    db.hmset('user:' + id, userObj, function(err){
       fn(err);
     });
   });
 };
 
-User.prototype.hashPassword = function(fn) {
+Users.prototype.hashPassword = function(fn) {
   var user = this;
   bcrypt.genSalt(12, function(err,salt){
     if(err) return fn(err);
@@ -50,13 +58,12 @@ User.prototype.hashPassword = function(fn) {
   })
 }
 
-var tobi = new User({
+var test = new Users({
   name:'xq',
   pass:'im a xq',
   age:'18'
 });
-
-tobi.save(function(err){
+test.save(function(err){
   if (err) throw err;
-  console.log('user id %d',xq.id);
+  console.log('user id %d',test.id);
 })
